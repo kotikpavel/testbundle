785bc27a910324e94b80b26b85fc5fa5c6c8ba21
kolomytkin <kolomytkin@imedicum.ru>
2015-09-23 19:45:47 +0400

    add items feature

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -2,12 +2,21 @@
 
 namespace Kotik\TestBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
-    {
-        return $this->render('KotikTestBundle:Default:index.html.twig', array('name' => $name));
-    }
+  public function indexAction($name)
+  {
+    return $this->render('KotikTestBundle:Default:index.html.twig', array('name' => $name));
+  }
+
+  /**
+   * @Template()
+   */
+  public function itemsAction()
+  {
+    return ['items' => ['one', 'two', 'three']];
+  }
 }
